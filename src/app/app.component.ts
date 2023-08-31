2b2c7a2d2c8b89a5bee85070904e89f293f6dace
Thierry Li <{ID}+{username}@users.noreply.github.com>
2023-08-31 15:38:35 +0200

    feat: starting Pokemon app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemons';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-learning';
+  name: string;
+  pokemonList: Pokemon[] = POKEMONS;
+
+  ngOnInit(): void {
+    console.log('HelloWorldComponent: ngOnInit', this.pokemonList);
+    this.selectPokemon(POKEMONS[0])
+  }
+
+  selectPokemon(pokemon: Pokemon) {
+    console.log(`Selected pokemon: ${pokemon.name}`);
+    this.name = pokemon.name
+  }
 }
